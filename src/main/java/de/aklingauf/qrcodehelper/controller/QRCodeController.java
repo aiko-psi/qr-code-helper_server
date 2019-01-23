d5a5ec2f982ca58f5648ae22b93c227ee05c372f
aiko-psi <blackaiko@gmail.com>
2019-01-23 16:42:11 +0100

    Fix Bug

--- src/main/java/de/aklingauf/qrcodehelper/controller/QRCodeController.java
+++ src/main/java/de/aklingauf/qrcodehelper/controller/QRCodeController.java
@@ -3,10 +3,13 @@ package de.aklingauf.qrcodehelper.controller;
 import de.aklingauf.qrcodehelper.exception.ResourceNotFoundException;
 import de.aklingauf.qrcodehelper.model.QRCode;
 import de.aklingauf.qrcodehelper.model.QRRedirect;
+import de.aklingauf.qrcodehelper.payload.ApiResponse;
 import de.aklingauf.qrcodehelper.repository.QRCodeRepository;
 import de.aklingauf.qrcodehelper.security.CurrentUser;
 import de.aklingauf.qrcodehelper.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -33,8 +36,35 @@ public class QRCodeController {
                 .orElseThrow(() -> new ResourceNotFoundException("QRCode", "qrCodeId", qrCodeId));
     }
 
+    //Check
+
+    @GetMapping("/qrcodes/check/exist/{qrCodeId}")
+    public ResponseEntity<?> checkQRExists(@PathVariable (value = "qrCodeId") Long qrCodeId){
+        if(qrCodeRepository.existsById(qrCodeId)){
+            return new ResponseEntity(new ApiResponse(true, "QR-Code verfügbar."),
+                    HttpStatus.resolve(200));
+        }else{
+            return new ResponseEntity(new ApiResponse(false, ""),
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/qrcodes/check/filled/{qrCodeId}")
+    public ResponseEntity<?> checkQRFilled(@PathVariable (value = "qrCodeId") Long qrCodeId){
+        return qrCodeRepository.findById(qrCodeId).map(qrCode -> {
+            if(qrCode.getRedirect() != null){
+                return new ResponseEntity(new ApiResponse(true, "QR-Code belegt."),
+                        HttpStatus.resolve(200));
+            }else{
+                return new ResponseEntity(new ApiResponse(false, "QR-Code nicht belegt."),
+                        HttpStatus.BAD_REQUEST);
+            }
+        }).orElseThrow(() -> new ResourceNotFoundException("QRCode", "qrCodeId", qrCodeId));
+    }
+
+
     // Post
-    @PostMapping("/qrcodes/")
+    @PostMapping("/qrcodes")
     public QRCode createQRCode(){
         QRCode qr = new QRCode();
         return qrCodeRepository.save(qr);
